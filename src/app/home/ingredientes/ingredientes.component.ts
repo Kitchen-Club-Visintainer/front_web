51d9528d19637ab0cacc783b55629d1f786c669e
eusoudavi <eusoudavi@gmail.com>
2023-09-27 01:37:49 -0300

    ajuste no cadastro de ingredientes

--- src/app/home/ingredientes/ingredientes.component.ts
+++ src/app/home/ingredientes/ingredientes.component.ts
@@ -3,69 +3,79 @@ import {GrupoAlimentar, GrupoAlimentarDesc} from "./GrupoAlimentar";
 import {AbstractControl, FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Ingrediente} from "./ingrediente.model";
 import {IngredientesService} from "./ingredientes.service";
+import {LoginService} from "../../login/login.service";
 
 @Component({
-    selector: 'app-ingredientes',
-    templateUrl: './ingredientes.component.html',
-    styleUrls: ['./ingredientes.component.scss']
+  selector: 'app-ingredientes',
+  templateUrl: './ingredientes.component.html',
+  styleUrls: ['./ingredientes.component.scss']
 })
 export class IngredientesComponent implements OnInit {
 
-    ingredientesForm: FormGroup;
-    gruposAlimentares: { codigo: number; descricao: string }[];
+  ingredientesForm: FormGroup;
+  gruposAlimentares: { codigo: number; descricao: string }[];
 
-    constructor(private formBuilder: FormBuilder, private ingredienteService: IngredientesService) {
-        this.gruposAlimentares = Object.values(GrupoAlimentar)
-            .filter(v => isNaN(Number(v)))
-            .map((item: string | GrupoAlimentar, indice) => ({
-                // codigo: (value as GrupoAlimentar),
-                codigo: item as GrupoAlimentar,
-                descricao: GrupoAlimentarDesc[indice != 8 ? indice + 1 : 99]
-            }));
+  constructor(
+    private formBuilder: FormBuilder,
+    private ingredienteService: IngredientesService,
+    private authService: LoginService) {
 
-        this.ingredientesForm = this.formBuilder.group({
-            ingNome: ['', Validators.required],
-            ingValorNut: ['', Validators.required],
-            ingGrupoAlimentar: [null, Validators.required]
-        });
+    this.gruposAlimentares = Object.values(GrupoAlimentar)
+      .filter(v => isNaN(Number(v)))
+      .map((item: string | GrupoAlimentar, indice) => ({
+        // codigo: (value as GrupoAlimentar),
+        codigo: item as GrupoAlimentar,
+        descricao: GrupoAlimentarDesc[indice != 8 ? indice + 1 : 99]
+      }));
+
+    this.ingredientesForm = this.formBuilder.group({
+      ingNome: ['', Validators.required],
+      ingValorNut: ['', Validators.required],
+      ingGrupoAlimentar: [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  get ingredientes(): { [key: string]: AbstractControl; } {
+    return this.ingredientesForm.controls;
+  }
+
+  cadastrar(): void {
+    let nome = this.ingredientes['ingNome'].value;
+    let valorNutricional = this.ingredientes['ingValorNut'].value;
+    let grupoAlimentar = this.ingredientes['ingGrupoAlimentar'].value;
+    let ingrediente: Ingrediente = new Ingrediente(nome, valorNutricional, grupoAlimentar);
+
+    this.ingredienteService.cadastrarIngredientes(ingrediente)
+      .subscribe({
+        next: (result: Ingrediente) => {
+          console.log(result)
+        },
+        error: (error) => {
+          this.verificarErroSessao(error);
+          console.log(error);
+        },
+        complete: () => {
+          console.log(nome + valorNutricional + grupoAlimentar);
+          this.cleanForm();
+        }
+      });
+  }
+
+  private verificarErroSessao(error: any): void {
+    if (error.status == 403) {
+      console.log(error.message);
+      this.authService.logout();
     }
+  }
 
-    ngOnInit() {
-    }
+  cleanForm(): void {
+    this.ingredientesForm.reset();
+  }
 
-    get ingredientes(): { [key: string]: AbstractControl; } {
-        return this.ingredientesForm.controls;
-    }
-
-    cadastrar(): void {
-        let nome = this.ingredientes['ingNome'].value;
-        let valorNutricional = this.ingredientes['ingValorNut'].value;
-        let grupoAlimentar = this.ingredientes['ingGrupoAlimentar'].value;
-        let ingrediente: Ingrediente = new Ingrediente(nome, valorNutricional, grupoAlimentar);
-
-      this.ingredienteService.cadastrarIngredientes(ingrediente)
-        .subscribe({
-          next: (result) => {
-            // Lide com a resposta bem-sucedida aqui
-            console.log(result);
-          },
-          error: (error) => {
-            // Lide com erros de solicitação (código de resposta não é 201, erro de rede, etc.) aqui
-            console.log(error);
-          },
-          complete: () => {
-            // Este bloco é acionado quando a solicitação é concluída (tanto para sucesso quanto para erro)
-            console.log("teste")
-          }
-        });
-
-      // .subscribe(value => {
-            //     console.log('Sucesso', value)
-            // }, error => {
-            //     console.log('Erro', error)
-            // });
-
-        console.log(nome + valorNutricional + grupoAlimentar)
-        // resetar formulário + recarregar a página
-    }
+  reloadPage(): void {
+    window.location.reload();
+  }
 }
--- src/app/home/ingredientes/ingredientes.service.ts
+++ src/app/home/ingredientes/ingredientes.service.ts
@@ -3,26 +3,25 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Ingrediente} from "./ingrediente.model";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment.prod";
-import {FactoryEndpointService} from "../../shared/services/factory-endpoint.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class IngredientesService {
 
-    private urlBase: string = `${environment.apiUrl}${environment.urlIngredientes}`;
+  private urlBase: string = `${environment.apiUrl}${environment.urlIngredientes}`;
 
-    httpOptions = {
-        headers: new HttpHeaders({'Content-Type': 'application/json'})
-    }
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
 
-    constructor(private httpClient: HttpClient, private factoryEndpointService: FactoryEndpointService) {
-    }
+  constructor(private httpClient: HttpClient) {
+  }
 
-    public cadastrarIngredientes(ingrediente: Ingrediente): Observable<any> {
-        return this.httpClient.post<Ingrediente>(
-            this.urlBase + `${environment.createNew}`,
-            JSON.stringify(ingrediente),
-            this.factoryEndpointService.httpOptions)
-    }
+  public cadastrarIngredientes(ingrediente: Ingrediente): Observable<Ingrediente> {
+    return this.httpClient.post<Ingrediente>(
+      this.urlBase,
+      ingrediente,
+      this.httpOptions)
+  }
 }
